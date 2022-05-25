d54c0945780a157b41b9d7493b5cb59a1a0c16e7
lauratro <lauratronchin@hotmail.it>
2022-05-25 13:00:42 +0200

    Import Post in Index.ts and add it in the init obj

--- index.ts
+++ index.ts
@@ -1,8 +1,9 @@
 import {MikroORM} from "@mikro-orm/core"
 import { __prod__ } from "./src/constants"
+import { Post } from "./src/entities/Post"
 const main =async () => {
     //if we are not in production I want debug to be on
-    const orm = await MikroORM.init({ dbName: "lireddit", type:"postgresql", debug: !__prod__, }) 
+    const orm = await MikroORM.init({ entities:[Post],dbName: "lireddit", type:"postgresql", debug: !__prod__, }) 
  
 }
 

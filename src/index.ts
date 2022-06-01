815b17bc886fd39bae4ab00e2fe4b6ab3796158c
lauratro <lauratronchin@hotmail.it>
2022-06-01 17:14:59 +0200

    import mikro-orm.config

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,17 @@
 import {MikroORM, RequiredEntityData} from "@mikro-orm/core"
 import { __prod__ } from "./constants"
 import { Post } from "./entities/Post"
+import mikroOrmConfig from "./mikro-orm.config"
 const main =async () => {
     //if we are not in production I want debug to be on
-    const orm = await MikroORM.init({ entities:[Post], dbName: "lireddit", type:"postgresql", debug: !__prod__, }) 
+    const orm = await MikroORM.init(mikroOrmConfig) 
 const post= orm.em.create(Post, {title:"test"} as RequiredEntityData<Post>);
+///add post to database
+await orm.em.persistAndFlush(post)
+
+
 }
 
-main()
+main().catch(err =>{
+    console.error(err)
+})
